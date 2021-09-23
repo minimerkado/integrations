7edee90a6584a1c4740e54fe8fc75c49b46c841e
Leone Parise <1442927+leoneparise@users.noreply.github.com>
2021-09-23 17:04:16 -0300

    Fix Facebook constants

--- src/Facebook/Catalog/Availability.php
+++ src/Facebook/Catalog/Availability.php
@@ -5,7 +5,9 @@ namespace Facebook\Catalog;
 abstract class Availability
 {
     const IN_STOCK = 'in stock';
-    const AVAILABLE = 'available';
+    const AVAILABLE = 'available for order';
     const FOR_ORDER = 'for order';
     const OUT_OF_STOCK = 'out of stock';
+    const DISCONTINUED = 'discontinued';
+    const PENDING = 'pending';
 }
